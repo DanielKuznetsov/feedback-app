10076c8aa763cada88208ec8a75419eaaddda26a
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-12-29 21:51:21 -0800

    Add InnerMain empty component with styles

--- feedback-app/src/Main.js
+++ feedback-app/src/Main.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./Main.scss"
 import Suggestion from './Suggestion'
+import InnerMain from './InnerMain'
 
 function Main() {
   return (
-    <div>
-      <Suggestion />
+    <div className='Main'>
+      {/* <Suggestion /> */}
+      <InnerMain />
     </div>
   )
 }
