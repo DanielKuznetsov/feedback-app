030d52dbc931abb3c06e965815dca359f8e7f50e
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-12-31 16:51:31 -0800

    Add styles to EditFeedback

--- feedback-app/src/EditFeedback.js
+++ feedback-app/src/EditFeedback.js
@@ -22,15 +22,16 @@ function EditFeedback() {
           <g fill="none" fillRule="evenodd">
             <circle fill="url(#a)" cx="28" cy="28" r="28" />
             <path
+              transform="translate(8 8)"
+              d="M19.512 15.367l4.975 4.53-3.8 5.54L11.226 29l4.485-4.1c.759.275 1.831.026 2.411-.594a1.958 1.958 0 00-.129-2.82c-.836-.745-2.199-.745-2.964.068-.57.607-.767 1.676-.44 2.381L11 28.713c.255-1.06.683-2.75 1.115-4.436l.137-.531c.658-2.563 1.287-4.964 1.287-4.964l5.973-3.415zM23.257 12L28 16.443l-2.584 2.606-4.89-4.583L23.257 12z"
               fill="#FFF"
               fillRule="nonzero"
-              d="M30.343 36v-5.834h5.686v-4.302h-5.686V20h-4.597v5.864H20v4.302h5.746V36z"
             />
           </g>
         </svg>
       </span>
       <div className="NewFeedback-wrapper">
-        <p className="title">EDITING New Feedback</p>
+        <p className="title">Editing New Feedback</p>
         <form className="form">
           <label className="label">
             <p className="label-title">Feedback Title</p>
@@ -56,6 +57,17 @@ function EditFeedback() {
           </label>
 
           <label className="label">
+            <p className="label-title">Update Status</p>
+            <p className="label-text">Change feedback state</p>
+            <select className="label-select">
+              <option value="Suggestion">Suggestion</option>
+              <option value="Planned">Planned</option>
+              <option value="In-Progress">In-Progress</option>
+              <option value="Live">Live</option>
+            </select>
+          </label>
+
+          <label className="label">
             <p className="label-title">Feedback Detail</p>
             <p className="label-text">
               Include any specific comments on what should be improved, added,
@@ -68,8 +80,11 @@ function EditFeedback() {
             />
           </label>
           <div className="form-buttons">
-            <div className="cancel">Cancel</div>
-            <input className="submit" type="submit" value="Add Feedback" />
+            <div className="delete">Delete</div>
+            <span>
+              <div className="cancel">Cancel</div>
+              <input className="submit" type="submit" value="Save Changes" />
+            </span>
           </div>
         </form>
       </div>
